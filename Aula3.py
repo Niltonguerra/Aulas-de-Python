34e892780c15b54ac1c17b76ca5db8dbc19b817f
Nilton <niltondg.30@outlook.com>
2022-01-04 11:29:37 -0300

    mensagem qualquer

--- Aula3.py
+++ Aula3.py
@@ -22,26 +22,63 @@
 # print(calculadora.subtracao())
 # print(calculadora.divisao())
 
+#
+# class Calculadora:                               # função retorna varialvel
+#     def __init__(self,):                         #metodo é não retorna varialvel
+#             pass                                 #como o init não pode ficar vazio, você coloca o pass( que é para passar)
+#                                                  #nesse caso não precisaria do init, mas vou deixar para fins educativos
+#
+#     def soma(self, valor_a, valor_b):
+#         return valor_a + valor_b
+#
+#     def subtracao(self, valor_a, valor_b):
+#         return valor_a - valor_b
+#
+#     def multiplicacao(self, valor_a, valor_b):
+#         return valor_a * valor_b
+#
+#     def divisao(self, valor_a, valor_b):
+#         return valor_a / valor_b
+#
+# calculadora = Calculadora()
+# print(calculadora.soma(10,2))
+# print(calculadora.subtracao(11,2))
+# print(calculadora.subtracao(25,5))
+# print(calculadora.divisao(100,25))
 
-class Calculadora:                               # função retorna varialvel
-    def __init__(self,):                         #metodo é não retorna varialvel
-            pass                                 #como o init não pode ficar vazio, você coloca o pass( que é para passar)
-                                                 #nesse caso não precisaria do init, mas vou deixar para fins educativos
 
-    def soma(self, valor_a, valor_b):
-        return valor_a + valor_b
 
-    def subtracao(self, valor_a, valor_b):
-        return valor_a - valor_b
+class Televisao:
+    def __init__(self):
+        self.canal = 5
+        self.ligado = False
 
-    def multiplicacao(self, valor_a, valor_b):
-        return valor_a * valor_b
+    def power(self):
+        if self.ligado:
+            self.ligado = False
+        else:
+            self.ligado = True
 
-    def divisao(self, valor_a, valor_b):
-        return valor_a / valor_b
+    def aumenta_canal(self):
+        self.canal += 1
 
-calculadora = Calculadora()
-print(calculadora.soma(10,2))
-print(calculadora.subtracao(11,2))
-print(calculadora.subtracao(25,5))
-print(calculadora.divisao(100,25))
+    def diminui_canal(self):
+        self.canal -= 1
+
+televisao = Televisao()
+print('a televisão está ligada {}'.format(televisao.ligado))
+televisao.power()
+print('a televisão está ligada {}'.format(televisao.ligado))
+print('qual é o canal da TV: {}'.format(televisao.canal))
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+televisao.aumenta_canal()
+print('qual é o canal agora: {}'.format(televisao.canal))
+televisao.diminui_canal()
+televisao.diminui_canal()
+televisao.diminui_canal()
+televisao.diminui_canal()
+televisao.diminui_canal()
+televisao.diminui_canal()
+print('qual é o canal agora então: {}'.format(televisao.canal))
+
